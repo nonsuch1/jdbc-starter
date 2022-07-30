453d7c6c32dc85da244796b37be3b23474b4c5b6
nonsuch1 <nonsuch.11111@gmail.com>
2022-07-30 14:08:44 +0300

    Create statement.

--- src/com/nonsuch1/jdbc/starter/jdbcRunner.java
+++ src/com/nonsuch1/jdbc/starter/jdbcRunner.java
@@ -4,14 +4,22 @@ import com.nonsuch1.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class jdbcRunner {
     public static void main(String[] args) throws SQLException {
-        Class<Driver> driverClass = Driver.class;
-        try (Connection connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                id SERIAL PRIMARY KEY,
+                data TEXT NOT NULL);
+                """;
+        try (Connection connection = ConnectionManager.open();
+                Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            System.out.println(connection.getSchema());
+            boolean executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
